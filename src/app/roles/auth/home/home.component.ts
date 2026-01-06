2222f27d712c9488997661273ac97dbf5887415c
L <2463716@cognizant.com>
2026-01-06 16:53:06 +0530

    Features and about

--- src/app/roles/auth/home/home.component.ts
+++ src/app/roles/auth/home/home.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { FeaturesComponent } from './features/features.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from "./about/about.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent,HeroComponent,FeaturesComponent,FooterComponent],
+  imports: [NavbarComponent, HeroComponent, FeaturesComponent, FooterComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
